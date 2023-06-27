582f0407b453bb1da69488047fddc9067c213f61
Digvijay Singh <dssodha1234@gmail.com>
2023-06-27 10:47:00 +0530

    Delete /dist and config Webpack to production Mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,45 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: 'development',
-    entry: {
-      index: './src/index.js',
-      home: './src/home/home.js',
-    },
-    devtool: 'inline-source-map',
-    devServer: {
-      static: './dist',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-          title: 'Ronin Ramen',
-          favicon:"./src/squid.png"
-        }),
-      ],
-     output: {
-      filename: '[name].bundle.js',
-       path: path.resolve(__dirname, 'dist'),
-       clean: true,
-     },
-     optimization: {
-      runtimeChunk: 'single',
-    },
-     module: {
-      rules: [
-        {
-          test: /\.css$/i,
-          use: ['style-loader', 'css-loader'],
-        },
-        {
-          test: /\.(png|svg|jpg|jpeg|gif)$/i,
-          type: 'asset/resource',
-          // use: ['file-loader']
-        },
-        {
-          test: /\.(woff|woff2|eot|ttf|otf)$/i,
-          type: 'asset/resource',
-        },
-      ],
-    },
-   };
+  mode: "production",
+  entry: {
+    index: "./src/index.js",
+    home: "./src/home/home.js",
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Ronin Ramen",
+      favicon: "./src/squid.png",
+    }),
+  ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  optimization: {
+    runtimeChunk: "single",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+        // use: ['file-loader']
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
